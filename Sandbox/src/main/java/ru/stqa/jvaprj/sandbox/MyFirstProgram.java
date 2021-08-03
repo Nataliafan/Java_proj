0727f2acc45df2feac64a2f347699e8352d6e523
Nataliafan <barba1@yandex.ru>
2021-08-03 18:27:08 +0300

    операция расчета расстояния между точками убрана в класс и создан метод расчета

--- Sandbox/src/main/java/ru/stqa/jvaprj/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/jvaprj/sandbox/MyFirstProgram.java
@@ -12,16 +12,18 @@ public class MyFirstProgram {
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и "+ r.b + " = " + r.area());
 		System.out.println("Разница между площадью квадрата со стороной "+ s.l +" и площадью прямоугольника со сторонами "+ r.a + " и " + r.b+ " = "+(s.area()-r.area()));
 
-		Point p1 = new Point(18, 30);
+		Point p1 = new Point(3, 6);
 
-		Point p2 = new Point(15, 25);
+		Point p2 = new Point(5, 8);
+
+		Distance dist = new Distance(p1,p2);
 
 		System.out.println("Расстояние между точками: "+
 				"точка 1: х: "+p1.x+", у: "+ p1.y+"; " +
-				"точка 2: х: "+p2.x +", У: "+p2.y +" = "+distance(p1,p2));
+				"точка 2: х: "+p2.x +", У: "+p2.y +" = "+ dist.distance(p1,p2));
 
 
-		double x1=16;
+		double x1=8;
 		System.out.println("Корень квадратный из "+x1+" = "+sqrt(x1));
 
 
@@ -33,9 +35,9 @@ public class MyFirstProgram {
 	public static double sqrt(double x1) {
 		return Math.sqrt(x1);
 	}
-	public static double distance(Point p1, Point p2){
-		double d = (p2.x- p1.x);
-		double k  = (p2.y-p1.y);
-		return Math.sqrt((d*d)+(k*k));
-	}
+//	public static double distance(Point p1, Point p2){
+//		double d = (p2.x- p1.x);
+//		double k  = (p2.y-p1.y);
+//		return Math.sqrt((d*d)+(k*k));
+//	}
 }
