b0923cb01bc4213c558a4e546f29920d7b10f610
Nataliafan <barba1@yandex.ru>
2021-08-03 13:10:28 +0300

    созданы методы квадрат и прямоугольник

--- Sandbox/src/main/java/ru/stqa/jvaprj/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/jvaprj/sandbox/MyFirstProgram.java
@@ -1,33 +1,23 @@
 package ru.stqa.jvaprj.sandbox;
 
-import java.sql.SQLOutput;
-
 public class MyFirstProgram {
 	public static void main(String[] args) {
 		hello("world");
 		hello("user");
 
-
 		Square s = new Square(5);
 
-		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
-
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
 		Rectangle r = new Rectangle(4,6);
-		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и "+ r.b + " = " + area(r));
-		System.out.println("Разница между площадью квадрата со стороной "+ s.l +" и площадью прямоугольника со сторонами "+ r.a + " и " + r.b+ " = "+(area(s)-area(r)));
-
-
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и "+ r.b + " = " + r.area());
+		System.out.println("Разница между площадью квадрата со стороной "+ s.l +" и площадью прямоугольника со сторонами "+ r.a + " и " + r.b+ " = "+(s.area()-r.area()));
 	}
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 
 	}
-	public static double area (Square s) {
-		return s.l*s.l;
-	}
-	public static double area (Rectangle r){
-		return r.a*r.b;
-	}
+
+
 
 }
