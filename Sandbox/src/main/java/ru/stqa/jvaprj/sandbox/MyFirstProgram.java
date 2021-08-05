a10438b228d0c516f528c02a2bdf77932b27708e
Nataliafan <barba1@yandex.ru>
2021-08-05 11:20:23 +0300

    Revert "добавлены возвращаемые функции вычисления площади квадрата и прямоугольника"
    This reverts commit 091ad87b82808f91fae2be4c566c0483e6f33065.

--- Sandbox/src/main/java/ru/stqa/jvaprj/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/jvaprj/sandbox/MyFirstProgram.java
@@ -1,33 +1,8 @@
 package ru.stqa.jvaprj.sandbox;
 
-import java.sql.SQLOutput;
-
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		hello("world");
-		hello("user");
-
-
-		double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-
-
-		double a = 4;
-		double b = 6;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b + " = " + area(a, b));
-		System.out.println("Разница между площадью квадрата со стороной "+ l +" и площадью прямоугольника со сторонами "+ a + " и " + b+ " = "+(area(l)-area(a,b)));
-
-
+	public static void main (String[] args){
+		String somebody ="world";
+		System.out.println ("Hello, " + somebody + "!");
 	}
-	public static void hello(String somebody) {
-		System.out.println("Hello, " + somebody + "!");
-
-	}
-	public static double area (double l) {
-		return l*l;
-	}
-	public static double area (double a, double b){
-		return a*b;
-	}
-
 }
