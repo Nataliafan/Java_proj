921aff18277c14edee5b3d6d9a2d4b6537f63b1c
Nataliafan <barba1@yandex.ru>
2021-08-05 11:19:41 +0300

    Revert "добавлено извлечение квадратного корня из числа и измерение расстояния между двумя точками"
    This reverts commit 20b0cdf2b4f55680f5ba1a4636c833fce1d6d279.

--- Sandbox/src/main/java/ru/stqa/jvaprj/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/jvaprj/sandbox/MyFirstProgram.java
@@ -6,36 +6,18 @@ public class MyFirstProgram {
 		hello("user");
 
 		Square s = new Square(5);
+
 		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
 		Rectangle r = new Rectangle(4,6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и "+ r.b + " = " + r.area());
 		System.out.println("Разница между площадью квадрата со стороной "+ s.l +" и площадью прямоугольника со сторонами "+ r.a + " и " + r.b+ " = "+(s.area()-r.area()));
-
-		Point p1 = new Point(18, 30);
-
-		Point p2 = new Point(15, 25);
-
-		System.out.println("Расстояние между точками: "+
-				"точка 1: х: "+p1.x+", у: "+ p1.y+"; " +
-				"точка 2: х: "+p2.x +", У: "+p2.y +" = "+distance(p1,p2));
-
-
-		double x1=16;
-		System.out.println("Корень квадратный из "+x1+" = "+sqrt(x1));
-
-
 	}
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 
 	}
-	public static double sqrt(double x1) {
-		return Math.sqrt(x1);
-	}
-	public static double distance(Point p1, Point p2){
-		double d = (p2.x- p1.x);
-		double k  = (p2.y-p1.y);
-		return Math.sqrt((d*d)+(k*k));
-	}
+
+
+
 }
