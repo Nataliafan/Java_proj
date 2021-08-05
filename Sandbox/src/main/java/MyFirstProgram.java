5041f38a1d04b1d07b2696415717084e9b8c9770
Nataliafan <barba1@yandex.ru>
2021-08-05 11:20:43 +0300

    Revert "добавлен пакет"
    This reverts commit 8d1099b781cd6b530597253726758cfc4532795b.

--- Sandbox/src/main/java/MyFirstProgram.java
+++ Sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,4 @@
-package ru.stqa.jvaprj.sandbox;
-
-public class MyFirstProgram {
+public class MyFirstProgram { 
 	public static void main (String[] args){
 		System.out.println ("Hello, world!");
 	}
