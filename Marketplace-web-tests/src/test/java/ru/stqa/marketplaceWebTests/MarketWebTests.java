d558c77f14d81ceb791237ad52d17903e477d1c8
Nataliafan <barba1@yandex.ru>
2021-08-10 22:08:06 +0300

    создан первый тест селениум

---
+++ Marketplace-web-tests/src/test/java/ru/stqa/marketplaceWebTests/MarketWebTests.java
@@ -0,0 +1,76 @@
+package ru.stqa.marketplaceWebTests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class MarketWebTests {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.gecko.driver", "C:\\Users\\Natalia\\Documents\\GitHub\\Repositories\\Java_proj\\Marketplace-web-tests\\drivers\\geckodriver.exe");
+    driver = new FirefoxDriver();
+    baseUrl = "https://market.yandex.ru/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testUntitledTestCase() throws Exception {
+    driver.get("https://market.yandex.ru/");
+    driver.findElement(By.id("header-search")).clear();
+    driver.findElement(By.id("header-search")).sendKeys("зеркало");
+    final String searchString = driver.findElement(By.id("header-search")).getText();
+    driver.findElement(By.cssSelector(".BDkvP")).submit();
+    driver.findElement(By.xpath("//button[@data-autotest-id = 'dprice']")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
